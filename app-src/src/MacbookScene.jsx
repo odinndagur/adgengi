f8cb73e74afa0fca10a9034da4bbc2205ec1e630
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-03-18 07:34:31 +0000

    deploy

--- app-src/src/MacbookScene.jsx
+++ app-src/src/MacbookScene.jsx
@@ -15,6 +15,7 @@ function Visir(){
     return (
         <div style={{
             filter:`contrast(${contrast})`,
+            webkitFilter:`contrast(${contrast})`,
             width:'100%',
             height:'100%',
             display:'flex',

89dfdc594d704cc44ade9f1a45e299da0e7abdca
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-04-07 17:13:58 +0000

    deploy

--- app-src/src/Hjakronunni.jsx
+++ app-src/src/Hjakronunni.jsx
@@ -8,6 +8,8 @@ import { useGLTF } from '@react-three/drei'
 
 export function HjaKronunni(props) {
   const { nodes, materials } = useGLTF('/hjakronunni-transformed.glb')
+  materials.Material_0.transparent = true
+  materials.Material_0.opacity = props.opacity
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Mesh_0.geometry} material={materials.Material_0} />

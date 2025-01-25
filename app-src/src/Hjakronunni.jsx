c9b354826d1e299d015bf97c7f89d7180bebd9fb
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2025-01-25 09:35:23 +0000

    deploy

--- app-src/src/Hjakronunni.jsx
+++ app-src/src/Hjakronunni.jsx
@@ -7,14 +7,14 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function HjaKronunni(props) {
-  const { nodes, materials } = useGLTF('/hjakronunni-transformed.glb')
-  materials.Material_0.transparent = true
-  materials.Material_0.opacity = props.opacity
-  return (
-    <group {...props} dispose={null}>
-      <mesh geometry={nodes.Mesh_0.geometry} material={materials.Material_0} />
-    </group>
-  )
+	const { nodes, materials } = useGLTF('/hjakronunni-transformed.glb')
+	materials.Material_0.transparent = true
+	materials.Material_0.opacity = props.opacity
+	return (
+		<group {...props} dispose={null}>
+			<mesh geometry={nodes.Mesh_0.geometry} material={materials.Material_0} />
+		</group>
+	)
 }
 
-useGLTF.preload('/hjakronunni-transformed.glb')
+useGLTF.preload('/adgengi/hjakronunni-transformed.glb')
--- app-src/src/Hjakronunnilit.jsx
+++ app-src/src/Hjakronunnilit.jsx
@@ -7,14 +7,14 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function HjaKronunniLit(props) {
-  const { nodes, materials } = useGLTF('/hjakronunnilit-transformed.glb')
-  materials.Material_0.transparent = true
-  materials.Material_0.opacity = props.opacity
-  return (
-    <group {...props} dispose={null}>
-      <mesh geometry={nodes.Mesh_0.geometry} material={materials.Material_0} />
-    </group>
-  )
+	const { nodes, materials } = useGLTF('/hjakronunnilit-transformed.glb')
+	materials.Material_0.transparent = true
+	materials.Material_0.opacity = props.opacity
+	return (
+		<group {...props} dispose={null}>
+			<mesh geometry={nodes.Mesh_0.geometry} material={materials.Material_0} />
+		</group>
+	)
 }
 
-useGLTF.preload('/hjakronunnilit-transformed.glb')
+useGLTF.preload('/adgengi/hjakronunnilit-transformed.glb')
